9b073f70d2aff34d069217aa9456a7e59495ecd6
Gautam Bhatnagar <gautam.bhatnagar@theboyknowsclass.com>
2018-02-04 13:32:28 +0000

    Inital Commit

---
+++ JiraPlanner/Controllers/MainViewModelController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using ReactiveUI;
+using TheBoyKnowsClass.JiraPlanner.ViewModels;
+
+namespace TheBoyKnowsClass.JiraPlanner.Controllers
+{
+    public static class MainViewModelController
+    {
+        public static void Initialize(MainViewModel mainViewModel)
+        {
+            mainViewModel.Email = Properties.Settings.Default.Email;
+            mainViewModel.Password = Properties.Settings.Default.Password;
+
+            mainViewModel.WhenAnyValue(vm => vm.Email).Subscribe(change =>
+            {
+                Properties.Settings.Default.Email = change;
+            });
+
+            mainViewModel.WhenAnyValue(vm => vm.Password).Subscribe(change =>
+            {
+
+                Properties.Settings.Default.Password = change;
+            });
+
+            mainViewModel.WhenAnyValue(vm => vm.SelectedProject).Subscribe(observer =>
+            {
+                if (observer == null)
+                {
+                    mainViewModel.IsProjectSelected = false;
+                    return;
+                }
+
+                mainViewModel.IsProjectSelected = true;
+
+                Properties.Settings.Default.Project = observer.Name;
+                Properties.Settings.Default.Save();
+                mainViewModel.ProjectVersions.Clear();
+
+                if (observer.ProjectVersions.Any())
+                {
+                    mainViewModel.ProjectVersions.AddRange(observer.ProjectVersions);
+                }
+
+                mainViewModel.SelectedProjectVersion = mainViewModel.ProjectVersions.FirstOrDefault(p => p.Name == Properties.Settings.Default.Version);
+
+            });
+
+            mainViewModel.WhenAnyValue(vm => vm.SelectedProjectVersion).Subscribe(observer =>
+            {
+                if (observer == null)
+                {
+                    mainViewModel.IsProjectVersionSelected = false;
+                    return;
+                }
+
+                Properties.Settings.Default.Version = observer.Name;
+                Properties.Settings.Default.Save();
+                mainViewModel.IsProjectVersionSelected = true;
+
+            });
+
+            var canConnect = mainViewModel.WhenAny(vm => vm.Email, vm => vm.Password, (e, p) => !(string.IsNullOrEmpty(e.Value) || string.IsNullOrEmpty(p.Value)));
+
+            mainViewModel.ConnectCommand = ReactiveCommand.Create(() => ConnectToJira(mainViewModel), canConnect);
+        }
+
+        private static void ConnectToJira(MainViewModel mainViewModel)
+        {
+            Properties.Settings.Default.Save();
+            mainViewModel.IsConnected = false;
+            mainViewModel.IsConnecting = true;
+            mainViewModel.IsProjectSelected = false;
+            mainViewModel.IsProjectVersionSelected = false;
+            Jira.SDK.Jira jira = new Jira.SDK.Jira();
+            jira.Connect(Properties.Settings.Default.Url, Properties.Settings.Default.Email, Properties.Settings.Default.Password);
+
+
+            mainViewModel.Projects.Clear();
+            mainViewModel.Projects.AddRange(jira.GetProjects());
+
+            mainViewModel.SelectedProject = mainViewModel.Projects.FirstOrDefault(p => p.Name == Properties.Settings.Default.Project);
+
+            mainViewModel.IsConnected = true;
+            mainViewModel.IsConnecting = false;
+        }
+    }
+}
---
+++ JiraPlanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Jira.SDK.Domain;
+using ReactiveUI;
+using TheBoyKnowsClass.JiraPlanner.Controllers;
+
+namespace TheBoyKnowsClass.JiraPlanner.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+        private string _email;
+        private string _password;
+        private Project _selectedProject;
+        private ProjectVersion _selectedProjectVersion;
+        private bool _isconnecting;
+        private bool _isconnected;
+        private bool _isProjectVersionSelected;
+        private bool _isProjectSelected;
+
+        public MainViewModel()
+        {
+            MainViewModelController.Initialize(this);
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => this.RaiseAndSetIfChanged(ref _email, value);
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+
+        public ReactiveCommand ConnectCommand { get; set; }
+
+        public ReactiveList<Project> Projects { get; } = new ReactiveList<Project>();
+
+        public Project SelectedProject
+        {
+            get => _selectedProject;
+            set => this.RaiseAndSetIfChanged(ref _selectedProject, value);
+        }
+
+        public bool IsProjectSelected
+        {
+            get => _isProjectSelected;
+            set => this.RaiseAndSetIfChanged(ref _isProjectSelected, value);
+        }
+
+        public ReactiveList<ProjectVersion> ProjectVersions { get; } = new ReactiveList<ProjectVersion>();
+
+        public ProjectVersion SelectedProjectVersion
+        {
+            get => _selectedProjectVersion;
+            set => this.RaiseAndSetIfChanged(ref _selectedProjectVersion, value);
+        }
+
+        public bool IsProjectVersionSelected
+        {
+            get => _isProjectVersionSelected;
+            set => this.RaiseAndSetIfChanged(ref _isProjectVersionSelected, value);
+        }
+
+        public bool IsConnecting
+        {
+            get => _isconnecting;
+            set => this.RaiseAndSetIfChanged(ref _isconnecting, value);
+        }
+
+        public bool IsConnected
+        {
+            get => _isconnected;
+            set => this.RaiseAndSetIfChanged(ref _isconnected, value);
+        }
+    }
+}
